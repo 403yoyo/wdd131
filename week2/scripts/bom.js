d3a11dea04248797d0c9024371c4bf180a7228ad
yoyo <etiuzalethankgod@gmail.com>
2025-05-23 11:59:23 +0100

    added some DOM handling

--- week2/scripts/bom.js
+++ week2/scripts/bom.js
@@ -1,18 +1,48 @@
+// const input = document.querySelector('#favchap');
+// const button = document.querySelector('button');
+// const list = document.querySelector('ul');
+
+// button.addEventListener('click', () => {
+//   const li = document.createElement('li');
+//   li.textContent = input.value;
+
+//   const deleteButton = document.createElement('button');
+//   deleteButton.textContent = '❌';
+
+//   deleteButton.setAttribute('aria-label', `Delete chapter ${input.value}`);
+
+//   li.append(deleteButton);
+//   list.append(li);
+// });
+
+
+
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('ul');
+const list = document.querySelector('#list');
 
 button.addEventListener('click', () => {
-  const li = document.createElement('li');
-  li.textContent = input.value;
+  if (input.value.trim() !== '') {
+    const li = document.createElement('li');
 
-  const deleteButton = document.createElement('button');
-  deleteButton.textContent = '❌';
+    li.textContent = input.value;
 
-  deleteButton.setAttribute('aria-label', `Delete chapter ${input.value}`);
 
-  li.append(deleteButton);
-  list.append(li);
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = '❌';
+    deleteButton.setAttribute('aria-label', `Remove ${input.value} from list`);
+
+    deleteButton.addEventListener('click', () => {
+      list.removeChild(li);
+      input.focus();
+    });
+
+    li.append(deleteButton);
+
+    list.append(li);
+
+    input.value = '';
+  }
+
+  input.focus();
 });
-
-
